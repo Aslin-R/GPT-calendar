93a161d0a9f946472863ace52f1e4c3a18c31ff2
aslin-r <aslinjoelr123@gmail.com>
2023-10-07 19:29:28 +0530

    going to finish

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-// JavaScript to populate calendar dates
+// JavaScript to populate calendar dates and navigate months
 
 const calendarDates = document.getElementById('calendar-dates');
 const currentYearElement = document.getElementById('current-year');
-const currentMonthElement = document.getElementById('current-month'); // Added for the month name
+const currentMonthElement = document.getElementById('current-month');
+
+const navArrows = document.querySelector('.nav-arrows');
+const prevMonthArrow = document.querySelector('.prev-month');
+const nextMonthArrow = document.querySelector('.next-month');
 
 const monthNames = [
     'January', 'February', 'March', 'April',
@@ -11,11 +15,12 @@ const monthNames = [
 ];
 
 const today = new Date();
-const currentYear = today.getFullYear();
-const currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+let currentMonth = today.getMonth();
+let currentDate = today.getDate();
 
 currentYearElement.textContent = currentYear;
-currentMonthElement.textContent = monthNames[currentMonth]; // Set the initial month name
+currentMonthElement.textContent = monthNames[currentMonth];
 
 function generateCalendar() {
     // Clear existing dates
@@ -33,8 +38,40 @@ function generateCalendar() {
     for (let day = 1; day <= daysInMonth; day++) {
         const dateCell = document.createElement('div');
         dateCell.textContent = day;
+        if (day === currentDate && currentMonth === today.getMonth() && currentYear === today.getFullYear()) {
+            dateCell.classList.add('current-date'); // Highlight the current date
+        }
         calendarDates.appendChild(dateCell);
     }
 }
 
 generateCalendar();
+
+// Function to navigate to the previous month
+function previousMonth() {
+    if (currentMonth === 0) {
+        currentMonth = 11;
+        currentYear--;
+    } else {
+        currentMonth--;
+    }
+    updateCalendar();
+}
+
+// Function to navigate to the next month
+function nextMonth() {
+    if (currentMonth === 11) {
+        currentMonth = 0;
+        currentYear++;
+    } else {
+        currentMonth++;
+    }
+    updateCalendar();
+}
+
+// Function to update the calendar when navigating
+function updateCalendar() {
+    currentYearElement.textContent = currentYear;
+    currentMonthElement.textContent = monthNames[currentMonth];
+    generateCalendar();
+}
